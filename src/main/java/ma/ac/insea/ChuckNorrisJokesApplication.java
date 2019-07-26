933767149b2bbbe70c258b88399e82a0d727fd1e
bahm2504 <mdoumaci@gmail.com>
2019-07-26 09:17:02 -0400

    project initialization

---
+++ src/main/java/ma/ac/insea/ChuckNorrisJokesApplication.java
@@ -0,0 +1,13 @@
+package ma.ac.insea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckNorrisJokesApplication.class, args);
+	}
+
+}
